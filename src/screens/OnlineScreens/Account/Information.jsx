aa3509e2d08c60bf0ae16b5b5d4f3645197209f7
claRa.git <rameau.claire.cr@gmail.com>
2025-04-28 14:26:16 +0200

    Page de modification du mdp

--- src/screens/OnlineScreens/Account/Information.jsx
+++ src/screens/OnlineScreens/Account/Information.jsx
@@ -9,6 +9,8 @@ import { API_URL, AVATAR_URL } from '../../../constants/apiConstant';
 import { FaPen } from 'react-icons/fa';
 import CustomInput from '../../../components/Ui/CustomInput';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { RiArrowLeftSFill } from 'react-icons/ri';
 
 const Information = () => {
 
@@ -93,104 +95,120 @@ const Information = () => {
   return (
     loading ? <PageLoader />
     :
-    <div className='flex flex-col items-center justify-center w-full h-full'>
-        <div className='flex flex-col items-center justify-center mb-4 relative'>
-            <img
-                src={ imgAvatar }
-                alt="Avatar"
-                className='w-48 h-48 rounded-full'
-            />
-            <FaPen
-                className='absolute top-2 right-2 bg-secondary-orange h-6 w-6 text-white rounded-sm p-1 cursor-pointer'
-                onClick={() => {
-                    setPath( imgAvatar );
-                    setIsEditingAvatar( true );
-                }}
-            />
-        </div>
-
-        <div className='flex flex-row items-center justify-center mb-4 '>
-            <div className='text-3xl font-bold' >
-                { userDetail.username }
+    <div className='flex flex-col items-center justify-center w-full h-full' >
+        <div className='flex w-full ml-4' >
+            <div className='flex justify-start items-center' >
+                <Link to={`/account/${userId}`} >
+                    <RiArrowLeftSFill
+                        size={30}
+                        className='text-white bg-secondary-pink rounded-lg  h-10 w-10 cursor-pointer'
+                    />
+                </Link>
             </div>
-            <FaPen
-                className='ml-2 bg-secondary-orange h-6 w-6 text-white rounded-sm p-1 cursor-pointer'
-                onClick={ () => {
-                    setNickname( userDetail.username );
-                    setIsEditing( true );
-                }}
-            />
+            <div className='flex w-full justify-center'>
+                <h2 className='m-8 text-5xl text-primary font-bold pr-10' >
+                    Modification du profil
+                </h2>
+            </div>
         </div>
-
-        { isEditing && 
-        (
-            <div className='z-30 absolute top-0 right-0 bottom-0 left-0 backdrop-blur flex items-center justify-center' >
-                <div className='flex flex-col relative w-full text-white sm:w-2/3 lg:w-1/2 h-1/2 rounded-2xl justify-center items-center bg-primary' >
-                    <h2 className='text-lg font-bold mb-4' >
-                        Modifier le nom
-                    </h2>
-                    <div className='flex flex-row text-primary align-center' >
-                        <CustomInput
-                            state={ nickname }
-                            label={ 'Nom' }
-                            type={ 'text' }
-                            callable={( e ) => setNickname( e.target.value ) }
-                        />
-                    </div>
-                    <div className='flex flex-col text-primary justify-center items-center' >
-                        <button 
-                            onClick={ handleEditName } 
-                            className='w-full bg-secondary-orange font-bold p-3 rounded-lg transition'
-                        >
-                            Sauvegarder
-                        </button>
-                        <button 
-                            onClick={ () => setIsEditing( false ) } 
-                            className='w-full bg-secondary-pink font-bold p-3 mt-2 rounded-lg transition'
-                        >
-                            Annuler
-                        </button>
+        
+        <div className='flex flex-col items-center justify-center rounded-lg bg-primary w-2/3 h-full p-16 text-white mb-16' >
+            <div className='flex flex-col items-center justify-center mb-4 relative' >
+                <img
+                    src={ imgAvatar }
+                    alt="Avatar"
+                    className='w-48 h-48 rounded-full'
+                />
+                <FaPen
+                    className='absolute top-2 right-2 bg-secondary-orange h-6 w-6 text-white rounded-sm p-1 cursor-pointer'
+                    onClick={() => {
+                        setPath( imgAvatar );
+                        setIsEditingAvatar( true );
+                    }}
+                />
+            </div>
+            <div className='flex flex-row items-center justify-center mb-4 '>
+                <div className='text-3xl font-bold' >
+                    { userDetail.username }
+                </div>
+                <FaPen
+                    className='ml-2 bg-secondary-orange h-6 w-6 text-white rounded-sm p-1 cursor-pointer'
+                    onClick={ () => {
+                        setNickname( userDetail.username );
+                        setIsEditing( true );
+                    }}
+                />
+            </div>
+            { isEditing &&
+            (
+                <div className='z-30 absolute top-0 right-0 bottom-0 left-0 backdrop-blur flex items-center justify-center' >
+                    <div className='flex flex-col relative w-full text-white sm:w-2/3 lg:w-1/2 h-1/2 rounded-2xl justify-center items-center bg-primary  border-2 border-secondary-orange' >
+                        <h2 className='text-lg font-bold mb-4' >
+                            Modifier le nom
+                        </h2>
+                        <div className='flex flex-row text-primary align-center' >
+                            <CustomInput
+                                state={ nickname }
+                                label={ 'Nom' }
+                                type={ 'text' }
+                                callable={( e ) => setNickname( e.target.value ) }
+                            />
+                        </div>
+                        <div className='flex flex-col text-primary justify-center items-center' >
+                            <button
+                                onClick={ handleEditName }
+                                className='w-full bg-secondary-orange font-bold p-3 rounded-lg transition'
+                            >
+                                Sauvegarder
+                            </button>
+                            <button
+                                onClick={ () => setIsEditing( false ) }
+                                className='w-full bg-secondary-pink font-bold p-3 mt-2 rounded-lg transition'
+                            >
+                                Annuler
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )}
-        { isEditingAvatar && 
-        (
-            <div className='z-30 absolute top-0 right-0 bottom-0 left-0 backdrop-blur flex items-center justify-center' >
-                <div className='flex flex-col relative w-full text-white sm:w-2/3 lg:w-1/2 h-1/2 rounded-2xl justify-center items-center bg-primary' >
-                    <h2 className='text-lg font-bold my-4' >
-                        Modifier l'avatar
-                    </h2>
-                    <div className="grid grid-cols-7 gap-5 p-5 grow place-content-center">
-                        {allAvatars.map((image) => (
-                            <div
-                                key={image.id}
-                                className={`p-2 hover:bg-gray-100 cursor-pointer ${isSelected === image.id ? 'bg-secondary-orange rounded-2xl' : ''}`}
-                                onClick={() => { setIsSelected(image.id); setPath(image['@id']); }}>
-                                <img
-                                    src={`${ AVATAR_URL }/${image.imagePath}`}
-                                    alt={`Avatar ${image.id}`}
-                                    className="w-12 h-12 rounded-full" />
-                            </div>
-                        ))}
-                    </div>
-                    <div className='flex flex-col text-primary justify-center items-center mb-4' >
-                        <button 
-                            onClick={ handleEditAvatar } 
-                            className='w-full bg-secondary-orange font-bold p-3 rounded-lg transition'
-                        >
-                            Sauvegarder
-                        </button>
-                        <button 
-                            onClick={ () => setIsEditingAvatar( false ) } 
-                            className='w-full bg-secondary-pink font-bold p-3 mt-2 rounded-lg transition'
-                        >
-                            Annuler
-                        </button>
+            )}
+            { isEditingAvatar &&
+            (
+                <div className='z-30 absolute top-0 right-0 bottom-0 left-0 backdrop-blur flex items-center justify-center' >
+                    <div className='flex flex-col relative w-full text-white sm:w-2/3 lg:w-1/2 h-1/2 rounded-2xl justify-center items-center bg-primary  border-2 border-secondary-orange' >
+                        <h2 className='text-lg font-bold my-4' >
+                            Modifier l'avatar
+                        </h2>
+                        <div className="grid grid-cols-7 gap-5 p-5 grow place-content-center">
+                            {allAvatars.map((image) => (
+                                <div
+                                    key={image.id}
+                                    className={`p-2 hover:bg-gray-100 cursor-pointer ${isSelected === image.id ? 'bg-secondary-orange rounded-2xl' : ''}`}
+                                    onClick={() => { setIsSelected(image.id); setPath(image['@id']); }}>
+                                    <img
+                                        src={`${ AVATAR_URL }/${image.imagePath}`}
+                                        alt={`Avatar ${image.id}`}
+                                        className="w-12 h-12 rounded-full" />
+                                </div>
+                            ))}
+                        </div>
+                        <div className='flex flex-col text-primary justify-center items-center mb-4' >
+                            <button
+                                onClick={ handleEditAvatar }
+                                className='w-full bg-secondary-orange font-bold p-3 rounded-lg transition'
+                            >
+                                Sauvegarder
+                            </button>
+                            <button
+                                onClick={ () => setIsEditingAvatar( false ) }
+                                className='w-full bg-secondary-pink font-bold p-3 mt-2 rounded-lg transition'
+                            >
+                                Annuler
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )}
+            )}
+        </div>
     </div>
   )
 }
--- src/screens/OnlineScreens/Account/Password.jsx
+++ src/screens/OnlineScreens/Account/Password.jsx
@@ -1,8 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthContext } from '../../../contexts/AuthContext';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import selectUserData from '../../../store/user/userSelector';
+import { fetchAllAvatars, fetchUserDetail } from '../../../store/user/userSlice';
+import PageLoader from '../../../components/Loader/PageLoader';
+import { API_URL, AVATAR_URL } from '../../../constants/apiConstant';
+import { FaEye, FaEyeSlash, FaPen } from 'react-icons/fa';
+import CustomInput from '../../../components/Ui/CustomInput';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { RiArrowLeftSFill } from 'react-icons/ri';
 
 const Password = () => {
+
+    // Récupération de l'utilisateur connecté
+    const { userId, username } = useAuthContext();
+
+    // Récupération de dispatch
+    const dispatch = useDispatch();
+
+    // Définition des states
+    const [ password, setPassword ] = useState( '' );
+    const [ isVisible, setIsVisible ] = useState( true );
+
+    useEffect(() => {
+      dispatch( fetchUserDetail( userId ) );
+    }, [ dispatch, userId ] );
+
+    // Récupération des informations de l'utilisateur
+    const { userDetail, loading } = useSelector( selectUserData );
+
+    // Mettre à jour le mot de passe de l'utilisateur
+    const handleEdit = async () => {
+        try {
+            // Mise à jour par le patch
+            axios.defaults.headers.patch[ 'Content-Type' ] = 'application/merge-patch+json';
+            const response = await axios.patch( `${ API_URL }/profiles/${ userId }`, {
+                password: password
+            });
+    
+            // Vérification de la réponse
+            if ( response.status === 200 ) {
+                dispatch( fetchUserDetail( userId ) );
+                setPassword( '' );
+            }
+        } catch ( error ) {
+            console.error( "Erreur lors de la mise à jour du mot de passe de l'utilisateur", error );
+        }
+    };
+
   return (
-    <div>Password</div>
+    loading ? <PageLoader />
+    :
+    <div className='flex flex-col items-center justify-center w-full h-full' >
+        <div className='flex w-full ml-4' >
+            <div className='flex justify-start items-center' >
+                <Link to={`/account/${userId}`} >
+                    <RiArrowLeftSFill
+                        size={30}
+                        className='text-white bg-secondary-pink rounded-lg  h-10 w-10 cursor-pointer'
+                    />
+                </Link>
+            </div>
+            <div className='flex w-full justify-center'>
+                <h2 className='m-8 text-5xl text-primary font-bold pr-10' >
+                    Modification du mot de passe
+                </h2>
+            </div>
+        </div>
+        
+        <div className='flex flex-col items-center justify-center rounded-lg bg-primary w-2/3 h-full p-16 text-primary mb-16' >
+            <div className='flex'>
+                <CustomInput
+                    state={ password }
+                    label={ 'Mot de passe' }
+                    type={ isVisible ? 'password' : 'text' }
+                    callable={ ( e ) => setPassword( e.target.value ) }
+                />
+                <div className='flex justify-center items-center p-2 mt-4'>
+                    { isVisible ?
+                        <FaEye
+                            size={ 20 }
+                            className='text-white'
+                            onClick={ () => setIsVisible( !isVisible ) }
+                        />
+                        :
+                        <FaEyeSlash
+                            size={ 20 }
+                            className='text-white'
+                            onClick={ () => setIsVisible( !isVisible ) }
+                        />
+                    }
+                </div>
+            </div>
+            <div className='flex flex-col justify-center items-center mb-4' >
+                <button
+                    onClick={ handleEdit }
+                    className='w-full bg-secondary-orange font-bold p-3 rounded-lg transition'
+                >
+                    Sauvegarder
+                </button>
+            </div>  
+        </div>
+                  
+    </div>
   )
 }
 
